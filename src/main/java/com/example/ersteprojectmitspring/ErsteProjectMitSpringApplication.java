0fecb565936415204f31c152c742b624ef00f914
Hozan9 <hozan.bahaden.93@gmail.com>
2023-03-17 09:58:45 +0100

    Initial commit

---
+++ src/main/java/com/example/ersteprojectmitspring/ErsteProjectMitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ersteprojectmitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsteProjectMitSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsteProjectMitSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ersteprojectmitspring/ErsteProjectMitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ersteprojectmitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErsteProjectMitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
